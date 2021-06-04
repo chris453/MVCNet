091927ebb86c1c8d0e4df86189c2e09dd417a278
chris453 <chris45300@hotmail.com>
2021-06-03 23:12:51 -0500

    added view portion

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -8,17 +8,27 @@ namespace MvcMovie.Controllers
         // 
         // GET: /HelloWorld/
 
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome()
+        // GET: /HelloWorld/Welcome/  
+        //user requests are routed to a controller 
+        //which work with the model to perform user actions or queries
+        //the conroller then choose the view to show the users based on request 
+        //both the view and controller depend on the model but not backwards
+        // every public method is a http endpoint 
+        //business logic should be in the model 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int numTimes = 1) // parameter numTimes =1 is default if not set in http
         {
-            return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
         }
+
+
     }
 }
